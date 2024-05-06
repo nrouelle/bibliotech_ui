7861ce6dded8ca7d3e0ad2615317bbb01390bbe2
nrouelle <consulting@nadegerouelle.fr>
2024-05-06 11:58:42 +0200

    use datetime on book

--- lib/src/library/book.dart
+++ lib/src/library/book.dart
@@ -1,16 +1,20 @@
 class Book {
-  Book(this.title, this.author, this.year, this.read);
+  Book(this.title, this.author, this.readDate, this.read);
 
   final String title;
   final String author;
-  final String year;
+  final DateTime readDate;
   final bool read;
 
   factory Book.fromJson(Map<String, dynamic> json) {
     return Book(json['title'] as String, json['author'] as String,
-        json['year'] as String, json['read'] as bool);
+        DateTime.parse(json['readDate']), json['read'] as bool);
   }
 
-  Map<String, dynamic> toJson() =>
-      {'title': title, 'author': author, 'year': year, 'read': true};
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'author': author,
+        'readDate': readDate.toIso8601String(),
+        'read': true
+      };
 }
